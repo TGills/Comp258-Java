e6d1a68c48f132077084d8ecab0299199967b241
TGills <tylerguillaume@live.com>
2016-12-20 10:31:34 -0600

    More Maze Stuff

--- KarlSaga.java
+++ KarlSaga.java
@@ -35,30 +35,34 @@ public class KarlSaga {
     LinkedList<Space> qv; 
     Stack<Space> path ;
     
-    public static void main(String[] args) {
-       
+    public static void main(String[] args) {       
         
         KarlSaga ks = new KarlSaga();
         ks.drawMap();
         ks.navigate(2,1);
         
-    }
-    
+    }    
     public KarlSaga(){
         q = new LinkedList<Space>();
         qv = new LinkedList<Space>();
         
         input = new Scanner(System.in);
-    }
-    
+    }    
     public void navigate(int i, int j){
+        addSpaces(maze1, i, j, new Space(i,j));
+        int x = 0;
+        while(!found){
+            x++;
+            Space s = q.removeLast();
+            qv.addFirst(s);
+            addSpaces(maze1, s.x, s.y, s);
+            
+        }
         
         
         
-        
-        
-    }
-    
+        System.out.println("Loops: " + x);
+    }    
     public void printPath(){
          System.out.println("path");
         while(path.size()>0){
@@ -67,16 +71,14 @@ public class KarlSaga {
              drawMap(stop.x,stop.y);
         }
         
-    }
-    
+    }    
     public void printQueue(){
         
         for (Space item : qv) {
             System.out.print(item);
         }
         
-    }
-    
+    }    
     public void drawMap(int ii, int jj ){
         
         for(int i=0; i<maze1.length; i++){
@@ -99,8 +101,7 @@ public class KarlSaga {
         
     }
        System.out.println("--------------------------------");
-    }
-    
+    }    
     public void drawMap(){
         
         for(int i=0; i<maze1.length; i++){
@@ -122,16 +123,28 @@ public class KarlSaga {
         
     }
      
-    }
-    
-    
+    }   
     public void addSpaces(int[][] maze, int i, int j, Space adder){
-        
+        for(int ii = -1; ii <= 1; ii++){
+            for(int jj = -1; jj <= 1; jj++){
+                if(!(ii == 0 && jj == 0))
+                if(maze[i + ii][j + jj] == 3){
+                    found = true;
+                    Space s = new Space(ii,jj);
+                    qv.addFirst(s);
+                }
+                else if(maze[i+ii][j+jj] == 0 && !found){
+                    Space s = new Space(ii,jj);
+                    if(!q.contains(s) && !qv.contains(s)){
+                        q.addFirst(s);
+                    }
+                }
+                
+                
+            }
+        }
         
     }
     
     
 }
-    
-    
-
