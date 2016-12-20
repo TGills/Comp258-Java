c6ed9bea553b16252d7692a4c131465f403eb5af
TGills <tylerguillaume@live.com>
2016-12-20 09:59:41 -0600

    Broke and Fixed KarlSaga

---
+++ KarlSaga - Starter/src/karlsaga/Space.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package karlsaga;
+
+/**
+ *
+ * @author OSBORNE
+ */
+public class Space {
+     public int x;
+     public int y;
+     
+     
+     public Space(){}
+     public Space(int x, int y){
+         this.x=x;
+         this.y=y;
+     }
+     
+     public boolean equals(Object s){
+         return ((Space)s).x == x && ((Space)s).y==y;
+     }
+     
+     public String toString(){
+         return "{"+x+","+y+"}";
+     }
+}
